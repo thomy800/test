7f6a5c962c70aaf45505ecc31c98bfa2b328a957
thomy800 <thomas.schwarz@campus.tu-berlin.de>
2013-07-05 16:39:22 -0400

    Update email.php

--- include/email.php
+++ include/email.php
@@ -1,8 +1,8 @@
 <?php
 	function sendMail($to, $subject,$message){
 		$headers = 'From: webmaster@example.com' . "\r\n" .
-		'Reply-To: webmaster@example.com' . "\r\n" .
-		'X-Mailer: PHP/' . phpversion();
+			'Reply-To: webmaster@example.com' . "\r\n" .
+			'X-Mailer: PHP/' . phpversion();
 
 		return mail($to, $subject, $message, $headers);
 	}
